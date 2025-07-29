1d066060569648d5e2c92636bfb09684c4a7d7af
Adolfo Suárez <ahssuarez@gmail.com>
2025-07-29 17:31:56 +0000

    Adelante

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="es">
       <body
         className={cn(
-          "min-h-screen bg-background font-body antialiased",
+          "min-h-screen font-body antialiased",
           fontBody.variable,
           fontHeadline.variable
         )}
