c8e9f6ef846b3b2d0948dc62031cbf317fd46d34
Adolfo Suárez <ahssuarez@gmail.com>
2025-07-29 22:17:49 +0000

    adelante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hola Mundo</h1>
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hola Mundo
+      </h1>
     </main>
   );
 }
