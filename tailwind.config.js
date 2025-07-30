928a320d8ff1c3f55d838cb7fad940eada348200
Adolfo Suárez <ahssuarez@gmail.com>
2025-07-30 21:29:20 +0000

    no recargo continuo todo igual

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
